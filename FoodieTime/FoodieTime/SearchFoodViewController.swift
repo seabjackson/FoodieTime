bb8f4f9ace78c0df291c3a83e57c7ae8d189e973
Seab <honeyboyseab@gmail.com>
2016-10-11 13:54:57 -0400

    allowed tableView rows to be seen that were hidden by the search bar

--- FoodieTime/FoodieTime/SearchFoodViewController.swift
+++ FoodieTime/FoodieTime/SearchFoodViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class SearchFoodViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // first lines of the table are being obscured by the search bar fix
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension SearchFoodViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
     }
-
-
 }
 
